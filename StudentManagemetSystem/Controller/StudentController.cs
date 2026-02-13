ba0809ff483ba6728b6d58650fb639f200f258d4
Abhishek dwivedi <Abhishekdwivedi4567@gmail.com>
2026-02-13 10:40:48 +0530

    Updated student delete and update method

--- StudentManagemetSystem/Controller/StudentController.cs
+++ StudentManagemetSystem/Controller/StudentController.cs
@@ -11,10 +11,11 @@ namespace StudentManagemetSystem.Controller
     {
         private readonly AppDbContext _repo;
 
-        public StudentController(AppDbContext repo) {
+        public StudentController(AppDbContext repo)
+        {
             _repo = repo;
-        
-        
+
+
         }
         [HttpGet]
         public async Task<IActionResult> GetStudentData()
@@ -22,7 +23,10 @@ namespace StudentManagemetSystem.Controller
             var student = await _repo.students.Include(s => s.studentFees)
                          .Select(s => new
                          {
-                             s.Id, s.Name, s.Address, Totalfee = s.studentFees.Sum(s => s.Totalfee),
+                             s.Id,
+                             s.Name,
+                             s.Address,
+                             Totalfee = s.studentFees.Sum(s => s.Totalfee),
                              totalPaid = s.studentFees.Sum(s => s.TotalPaid),
                              status =
                              (s.studentFees.Sum(f => (decimal?)f.Totalfee) ?? 0) == 0 ? "No fee" : (s.studentFees.Sum(f => (decimal?)f.TotalPaid) ?? 0) == 0 ? "Unpaid" :
@@ -34,13 +38,46 @@ namespace StudentManagemetSystem.Controller
             return Ok(student);
 
         }
-
+        [HttpPost]
         public async Task<IActionResult> AddStudent(Student st)
         {
-         _repo.students.Add(st);
+            _repo.students.Add(st);
             await _repo.SaveChangesAsync();
             return Ok(st);
         }
-       
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var s = await _repo.students.FindAsync(id);
+
+            if (s == null)
+            
+                return NotFound("Student Not Found");
+            
+            _repo.students.Remove(s);
+            await _repo.SaveChangesAsync();
+            return Ok("Delete Successfull");
+
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id , Student s)
+        {
+            if(id != s.Id)
+            
+                return BadRequest("Id Do not Match");
+            var su = await _repo.students.FindAsync(id);
+            if (su == null)
+                return NotFound("Student Not found");
+
+            su.Name = s.Name;
+            su.Address = s.Address;
+            su.Class = s.Class;
+            await _repo.SaveChangesAsync();
+            return Ok("Updated");
+
+
+
+        }
     }
 }
